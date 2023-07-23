d48b22f17c0ba41216f080ff15851a7fc49612ce
João Moraes <joaovitordmoraes@gmail.com>
2023-07-23 16:24:23 -0300

    Feat: Create Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,84 @@
-import { useEffect, useState } from "react";
-import { Divider, Input, Col, Row, Radio } from "antd";
+import { useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Divider, Input, Col, Row, Radio } from 'antd'
+import { BeersContext } from 'contexts/BeersContext'
+import { ProductCard } from 'components/ProductCard'
 
 import * as S from './styles'
 
-type beerCategoryProps = {
+type BeerCategoryProps = {
   [key: string]: string
+}
+
+type BeerResponseProps = {
+  id: number
+  name: string
+  tagline: string
+  first_brewed: string
+  description: string
+  image_url: string
+  abv: number
+  ibu: number
+  target_fg: number
+  target_og: number
+  ebc: number
+  srm: number
+  ph: number
+  attenuation_level: number
+  volume: {
+    value: number
+    unit: string
+  }
+  boil_volume: {
+    value: number,
+    unit: string
+  }
+  method: {
+    mash_temp: {
+      temp: {
+        value: number
+        unit: string
+      }
+      duration: number
+    }[]
+    fermentation: {
+      temp: {
+        value: number,
+        unit: number
+      }
+    },
+    twist: string | null
+  }
+  ingredients: {
+    malt: {
+      name: string
+      amount: {
+        value: number
+        unit: string
+      }
+    }[]
+    hops: {
+      name: string
+      amount: {
+        value: number
+        unit: string
+      }
+      add: string
+      attribute: string
+    }[]
+    yeast: string
+  }
+  food_pairing: string[]
+  brewers_tips: string
+  contributed_by: string
 }
 
 export function Home() {
   const [searchCategory, setSearchCategory] = useState('beer_name')
   const [search, setSearch] = useState('')
+  const [beers, setBeers] = useState<BeerResponseProps[]>([])
+  const { setBeer } = useContext(BeersContext)
+  const navigate = useNavigate()
 
   const { Search } = Input
   const RadioGroup = Radio.Group
@@ -21,7 +90,7 @@ export function Home() {
       const response = await fetch(`https://api.punkapi.com/v2/beers${populateReq}`);
       const beers = await response.json();
   
-      console.log(beers)
+      setBeers(beers)
     }
 
     getBeers()
@@ -34,7 +103,7 @@ export function Home() {
     { label: 'Food Pairing', value: 'food' }
   ]
 
-  const beerFilterCategory: beerCategoryProps = {
+  const beerFilterCategory: BeerCategoryProps = {
     'beer_name': 'Name',
     'malt': 'Malt',
     'hops': 'Hops',
@@ -42,17 +111,48 @@ export function Home() {
   }
 
   return (
-    <S.Container>
-      <Row>
-        <Col span={6}>
-          <S.Sidebar>
+    <S.Main>
+      <S.Container>
+        <Row>
+          <Col span={24}>
             <S.SearchTitle>Searching beers by {beerFilterCategory[searchCategory]}</S.SearchTitle>
             <Search placeholder="Search for the perfect beer" onSearch={(value) => setSearch(value)}/>
             <Divider />
             <RadioGroup options={beerFilterOptions} defaultValue={searchCategory} onChange={(event) => setSearchCategory(event.target.value)} />
-          </S.Sidebar>
-        </Col>
-      </Row>
-    </S.Container>
+          </Col>
+        </Row>
+        <Row>
+        <Col span={24}>
+          <Divider />
+          <S.ProductGrid>
+            {beers?.map((beer) => {
+              const slug = beer.name.replace(/[^\w-:]/g, '-').toLowerCase()
+
+              function handleClick() {
+                setBeer({
+                  name: beer.name,
+                  tagline: beer.tagline,
+                  description: beer.description,
+                  image: beer.image_url,
+                  food_pairing: beer.food_pairing
+                })
+                navigate(`/beer/${slug}`)
+              }
+
+              return (
+                <ProductCard
+                  key={beer.name}
+                  name={beer.name}
+                  description={beer.tagline}
+                  img={beer.image_url}
+                  onButtonClick={handleClick}
+                />
+              )
+            })}
+          </S.ProductGrid>
+          </Col>
+        </Row>
+      </S.Container>
+    </S.Main>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,8 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+export const Main = styled.main`
+  padding: 48px 24px;
+`
 
 export const Container = styled.div`
   max-width: 1360px;
@@ -6,8 +10,17 @@ export const Container = styled.div`
   margin: 0 auto;
 `
 
-export const Sidebar = styled.aside``
-
 export const SearchTitle = styled.h1`
   margin-bottom: 24px;
+`
+
+export const ProductGrid = styled.section`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 8px;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  }
 `
